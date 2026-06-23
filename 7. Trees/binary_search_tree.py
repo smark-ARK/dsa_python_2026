45ba4bbefe664130ab749fdafbec3acf56db3d84
smark-ark <arkhangreat2@gmail.com>
2026-06-23 01:54:04 +0000

    BST delete

--- 7. Trees/binary_search_tree.py
+++ 7. Trees/binary_search_tree.py
@@ -8,11 +8,56 @@ class BinarySearchTree:
     def __init__(self):
         self.root = None
 
-    def insert(self, value): ...
+    def insert(self, x):
+        def _insert(node, value):
+            if node is None:
+                return self._Node(value)
+            if value < node.vlue:
+                node.left = _insert(node.left, value)
+            if value > node.value:
+                node.right = _insert(node.right, value)
+            return node
 
-    def delete(self, value): ...
+        self.root = _insert(self.root, value=x)
 
-    def search(self, value): ...
+    def delete(self, x):
+        def _delete(node, value):
+            if node is None:
+                return None
+            if value < node.value:
+                node.left = _delete(node.left, value)
+            elif value > node.value:
+                node.right = _delete(node.right, value)
+            else:
+                if (node.left is None) and (node.right is None):
+                    return None
+                elif node.left is None:
+                    return node.right
+                elif node.right is None:
+                    return node.left
+                else:
+                    succ = node.right
+                    while succ.left:
+                        succ = succ.left
+                    node.value = succ.value
+                    node.right = _delete(node.right, succ.value)
+
+            return node
+
+        self.root = _delete(self.root, x)
+
+    def search(self, x):
+        def _search(node, value):
+            if node is None:
+                return False
+            if value < node.value:
+                return _search(node.left, value)
+            elif value > node.value:
+                return _search(node.right, value)
+            else:
+                return True
+
+        return _search(self.root, x)
 
     def contains(self, value) -> bool: ...
 
